73c60df158d45f2b969ce618479baa8c40be9bcc
unknown <calderamiguel36@gmail.com>
2025-11-26 14:27:21 -0400

    first commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Real Estate",
+    "summary": "Manage real estate properties and advertisements",
+    "website": "https://www.odoo.com",
+    "category": "Sales/Real Estate",
+    "author": "Miguel Caldera",
+    "version": "18.0.1.0.0",
+    "application": True,
+    "installable": True,
+    "depends": ["base"], #dependencias del módulo
+    "data": [
+        # Aquí iremos añadiendo views, menus, security, etc.
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
